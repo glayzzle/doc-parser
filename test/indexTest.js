7e36baf20511d67f54574c3829768e10d8410f52
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-01-05 20:36:49 +0100

    start tests

---
+++ test/indexTest.js
@@ -0,0 +1,14 @@
+/*!
+ * Copyright (C) 2017 Glayzzle (BSD3 License)
+ * @authors https://github.com/glayzzle/docblock-parser/graphs/contributors
+ * @url http://glayzzle.com/docblock-parser
+ */
+var DocBlockParser = require('../src/index');
+
+describe('Test API', function() {
+  var doc = new DocBlockParser();
+  it('should parse something', function() {
+    var ast = doc.parse('Hello world');
+    console.log(ast);
+  });
+});
