1b4c39cdeec9c63725e8159eec8027f2512f4943
Ioan CHIRIAC <i.chiriac@yahoo.com>
2017-02-21 23:47:45 +0100

    start a typescript definition

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+/*!
+ * Copyright (C) 2017 Glayzzle (BSD3 License)
+ * @authors https://github.com/glayzzle/php-parser/graphs/contributors
+ * @url http://glayzzle.com
+ */
+
+declare module "doc-parser" {
+
+    export interface Tag {
+        /** @todo from doc-parser */
+    }
+
+
+    export interface Annotation {
+        /** @todo from doc-parser */
+    }
+}
